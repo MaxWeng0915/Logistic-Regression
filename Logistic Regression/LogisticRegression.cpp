c976389fbd1ebb16c4f7428d4a6a3ebf85393285
MaxWeng0915 <aa014289@gmail.com>
2025-05-06 15:54:58 +0800

    update project: improved logistic regression algorithm

--- Logistic Regression/LogisticRegression.cpp
+++ Logistic Regression/LogisticRegression.cpp
@@ -4,16 +4,112 @@
 #include <vector>
 #include <string>
 #include <map>
-
+#include <cmath>
 
 using namespace std;
 
+class LogisticRegression {
+public:
+	vector<double> weights;
+	double bias = 0.0;
+	double learning_rate = 0.1;
+	int max_iter = 20000;
+	double tol = 1e-6;
+
+	LogisticRegression(int feature_size, double lr = 0.1, int max_it = 20000, double tolerance = 1e-6)
+		: learning_rate(lr), max_iter(max_it), tol(tolerance) {
+		weights = vector<double>(feature_size, 0.01);
+		bias = 0.0;
+	}
+
+	static double sigmoid(double x) {
+		return 1.0 / (1.0 + exp(-x));
+	}
+
+	double compute_cost(const vector<vector<double>>& x, const vector<double>& y) {
+		double cost = 0.0;
+		for (int i = 0; i < x.size(); i++) {
+			double z = bias;
+			for (int j = 0; j < weights.size(); j++) {
+				z += weights[j] * x[i][j];
+			}
+			double pred = sigmoid(z);
+			cost += -y[i] * log(pred) - (1 - y[i]) * log(1 - pred);
+		}
+		return cost / y.size();
+	}
+
+	void compute_gradient(const vector<vector<double>>& x, const vector<double>& y, vector<double>& w_grad, double& b_grad) {
+		int n = x.size();
+		int d = weights.size();
+		w_grad.assign(d, 0.0);
+		b_grad = 0.0;
+
+		for (int i = 0; i < n; i++) {
+			double z = bias;
+			for (int j = 0; j < d; j++) z += weights[j] * x[i][j];
+			double pred = sigmoid(z);
+			double diff = pred - y[i];
+
+			for (int j = 0; j < d; j++) {
+				w_grad[j] += x[i][j] * diff;
+			}
+			b_grad += diff;
+		}
+
+		for (int j = 0; j < d; j++) w_grad[j] /= n;
+		b_grad /= n;
+	}
+
+	void fit(const vector<vector<double>>& x, const vector<double>& y) {
+		double prev_cost = compute_cost(x, y);
+
+		for (int i = 0; i < max_iter; i++) {
+			vector<double> grad_w(weights.size(), 0.0);
+			double grad_b = 0.0;
+			compute_gradient(x, y, grad_w, grad_b);
+
+			for (int j = 0; j < weights.size(); j++)
+				weights[j] -= learning_rate * grad_w[j];
+			bias -= learning_rate * grad_b;
+
+			double cost = compute_cost(x, y);
+			if (i % 1000 == 0)
+				cout << "Epoch: " << i << " Cost: " << cost << endl;
+
+			if (abs(prev_cost - cost) < tol) {
+				cout << "Early stopping at iteration " << i << ", cost change: " << abs(prev_cost - cost) << endl;
+				break;
+			}
+			prev_cost = cost;
+		}
+	}
+
+	vector<double> predict_proba(const vector<vector<double>>& x) {
+		vector<double> probs;
+		for (const auto& row : x) {
+			double z = bias;
+			for (int j = 0; j < weights.size(); j++) {
+				z += weights[j] * row[j];
+			}
+			probs.push_back(sigmoid(z));
+		}
+		return probs;
+	}
+
+	vector<int> predict(const vector<vector<double>>& x, double threshold = 0.5) {
+		vector<double> probs = predict_proba(x);
+		vector<int> preds;
+		for (double p : probs) {
+			preds.push_back(p >= threshold ? 1 : 0);
+		}
+		return preds;
+	}
+};
+
+
 struct DataRecord {
-	double Age;
-	double Weight;
-	double BloodSugar;
-	double Gender;
-	double Diabetes;
+	double Age, Weight, BloodSugar, Gender, Diabetes;
 };
 
 int loadcsvfile2(string fname, map<string, vector<double>>& datas) {
@@ -24,34 +120,18 @@ int loadcsvfile2(string fname, map<string, vector<double>>& datas) {
 	}
 
 	string line;
-	vector<DataRecord> data;
+	getline(inputFile, line); // Skip header
+	while (getline(inputFile, line)) {
+		stringstream ss(line);
+		string token;
+		DataRecord record;
 
-	if (getline(inputFile, line)) {
-		while (getline(inputFile, line)) {
-			stringstream ss(line);
-			DataRecord record;
-			char comma;
-			ss >> record.Age >> comma;
-			ss >> record.Weight >> comma;
-			ss >> record.BloodSugar >> comma;
-			string _Gender;
-			getline(ss, _Gender, ',');
-			if ("M" == _Gender)
-				record.Gender = 1;
-			else 
-				record.Gender = 0;
-			ss >> record.Diabetes;
-			data.push_back(record);
-		}
-	}
+		getline(ss, token, ','); record.Age = stod(token);
+		getline(ss, token, ','); record.Weight = stod(token);
+		getline(ss, token, ','); record.BloodSugar = stod(token);
+		getline(ss, token, ','); record.Gender = (token == "M") ? 1 : 0;
+		getline(ss, token, ','); record.Diabetes = stod(token);
 
-	datas["Age"] = vector<double>();
-	datas["Weight"] = vector<double>();
-	datas["BloodSugar"] = vector<double>();
-	datas["Gender"] = vector<double>();
-	datas["Diabetes"] = vector<double>();
-
-	for (const DataRecord& record : data) {
 		datas["Age"].push_back(record.Age);
 		datas["Weight"].push_back(record.Weight);
 		datas["BloodSugar"].push_back(record.BloodSugar);
@@ -59,166 +139,58 @@ int loadcsvfile2(string fname, map<string, vector<double>>& datas) {
 		datas["Diabetes"].push_back(record.Diabetes);
 	}
 
-	inputFile.close();
 	return 1;
 }
 
-double sigmoid(double x) {
-	return 1.0 / (1.0 + exp(-x));
-}
-
-double relu(double x) {
-	return (x > 0) ? x : 0;
-}
-
-double compute_cost(vector<vector<double>> _x, vector<double> _y, vector<double> w, float b) {
-	vector<double> y_pred;
-
-	int size = w.size();
-	for (int i = 0; i < _y.size(); i++) {
-
-		double y = _y[i];
-		double z = b;
-		for (int j = 0; j < size; j++) {
-			z += w[j] * _x[i][j];
-		}	
-		y_pred.push_back(sigmoid(z));
-
-
-	}
-
+double mean(const vector<double>& data) {
 	double sum = 0.0;
-	for (int i = 0; i < _y.size(); i++) {
-		double cost = -_y[i] * log(y_pred[i]) - (1 - _y[i]) * log(1 - y_pred[i]);
-		sum += cost;
-	}
-	sum = sum / _y.size();
-
-	return sum;
-}
-
-void compute_gradient(vector<vector<double>> _x, vector<double> _y, vector<double> w, float b, vector<float>& _w_gradient, float& _b_gradient) {
-	int size = w.size();
-	vector<float> sumw(size, 0.0);
-	float sumb = 0.0;
-	for (int i = 0; i < _x.size(); i++) {
-
-		float y = _y[i];
-		vector<float> w_gradients(size, 0.0);
-
-		// y_pred = w1 * x1 + ... + wn *xn + b
-		float y_pred = b;
-		for (int j = 0; j < size; j++) {
-			y_pred += w[j] * _x[i][j];
-		}
-		y_pred = sigmoid(y_pred);
-
-		for (int j = 0; j < size; j++) {
-			w_gradients[j] = _x[i][j] * (y_pred - y);
-			sumw[j] += w_gradients[j];
-		}
-		float b_gradient = (y_pred - y);
-		sumb += b_gradient;
-	}
-
-	for (int j = 0; j < size; j++)
-		_w_gradient[j] = sumw[j] / _x.size();
-	_b_gradient = sumb / _x.size();
-}
-
-
-
-double calculateMean(const vector<double>& data) {
-	double sum = 0.0;
-	for (double value : data) {
-		sum += value;
-	}
+	for (double v : data) sum += v;
 	return sum / data.size();
 }
 
-double calculateStandardDeviation(const vector<double>& data) {
-	double mean = calculateMean(data);
-	double sumOfSquaredDifferences = 0.0;
-
-	for (double value : data) {
-		double difference = value - mean;
-		sumOfSquaredDifferences += difference * difference;
-	}
-
-	double variance = sumOfSquaredDifferences / data.size();
-	double standardDeviation = sqrt(variance);
-
-	return standardDeviation;
+double stddev(const vector<double>& data) {
+	double m = mean(data), sum = 0.0;
+	for (double v : data) sum += (v - m) * (v - m);
+	return sqrt(sum / data.size());
 }
 
-
-vector<double> featureScaling(vector<double> data) {
-	double mean = calculateMean(data);
-	double sd = calculateStandardDeviation(data);
+vector<double> featureScaling(const vector<double>& data) {
+	double m = mean(data), sd = stddev(data);
+	vector<double> scaled(data.size());
 	for (int i = 0; i < data.size(); i++)
-		data[i] = (data[i] - mean) / sd;
-
-	return data;
-
+		scaled[i] = (data[i] - m) / sd;
+	return scaled;
 }
 
-int main()
-{
+int main() {
 	map<string, vector<double>> datas;
-	int res = loadcsvfile2("Diabetes_Data.csv", datas);
-	if (res == 1) {
+	if (!loadcsvfile2("Diabetes_Data.csv", datas)) return -1;
 
-		datas["Age"] = featureScaling(datas["Age"]);
-		datas["Weight"] = featureScaling(datas["Weight"]);
-		datas["BloodSugar"] = featureScaling(datas["BloodSugar"]);
-		datas["Gender"] = featureScaling(datas["Gender"]);
+	datas["Age"] = featureScaling(datas["Age"]);
+	datas["Weight"] = featureScaling(datas["Weight"]);
+	datas["BloodSugar"] = featureScaling(datas["BloodSugar"]);
 
-
-		vector<double> w = { 1, 2, 3, 4 };
-		float b = 1.0;
-		vector<vector<double>> x;
-		for (int i = 0; i < datas["Age"].size(); i++)
-		{
-			vector<double> x_sub;
-			x_sub.push_back(datas["Age"][i]);
-			x_sub.push_back(datas["Weight"][i]);
-			x_sub.push_back(datas["BloodSugar"][i]);
-			x_sub.push_back(datas["Gender"][i]);
-			x.push_back(x_sub);
-
-			/*cout << "i: " << i << " Age: " << datas["Age"][i] << endl;
-			cout << "i: " << i << " Weight: " << datas["Weight"][i] << endl;
-			cout << "i: " << i << " BloodSugar: " << datas["BloodSugar"][i] << endl;
-			cout << "i: " << i << " Gender: " << datas["Gender"][i] << endl;*/
-		}
-
-		//double cost = compute_cost(x, datas["Diabetes"], w, b);		
-		//cout << " Cost: " << cost << endl;
-
-		float learning_rate = 0.1;
-		float cost;
-
-		for (int i = 0; i < 20000; i++) {
-			vector<float> w_gradient = { 0.0, 0.0, 0.0, 0.0 };
-			float b_gradient;
-			compute_gradient(x, datas["Diabetes"], w, b, w_gradient, b_gradient);
-
-			w[0] = w[0] - w_gradient[0] * learning_rate;
-			w[1] = w[1] - w_gradient[1] * learning_rate;
-			w[2] = w[2] - w_gradient[2] * learning_rate;
-			w[3] = w[3] - w_gradient[3] * learning_rate;
-			b = b - b_gradient * learning_rate;
-
-			cost = compute_cost(x, datas["Diabetes"], w, b);
-
-			if (i % 1000 == 0)
-				cout << "i: " << i << " Cost: " << cost << " w[0]:" << w[0] << " b:" << b << " w_gradient[0]:" << w_gradient[0] << " b_gradient:" << b_gradient << endl;
-		}		
-
-		
+	vector<vector<double>> x;
+	for (int i = 0; i < datas["Age"].size(); i++) {
+		x.push_back({
+			datas["Age"][i],
+			datas["Weight"][i],
+			datas["BloodSugar"][i],
+			datas["Gender"][i]
+			});
 	}
 
+	vector<double> y = datas["Diabetes"];
 
-	system("pause");
+	LogisticRegression model(4, 0.1, 20000, 1e-6);
+	model.fit(x, y);
+
+	auto predictions = model.predict(x);
+	cout << "Sample predictions:\n";
+	for (int i = 0; i < 5; i++) {
+		cout << "Actual: " << y[i] << ", Predicted: " << predictions[i] << endl;
+	}
+
+	cin.get();
 	return 0;
 }
